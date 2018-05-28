00ed9d40f70332688a8447f1db782ee827be3d91
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2018-05-29 05:21:42 +0900

    work around slashed symlink problem (Bug#31305) on OS X 10.9 and earlier

--- src/dired.c
+++ src/dired.c
@@ -814,9 +814,15 @@ static bool
 file_name_completion_dirp (int fd, struct dirent *dp, ptrdiff_t len)
 {
   USE_SAFE_ALLOCA;
+#if defined (DARWIN_OS) && !defined (HAVE_FACCESSAT)
+  char *subdir_name = SAFE_ALLOCA (len + 4);
+  memcpy (subdir_name, dp->d_name, len);
+  strcpy (subdir_name + len, "/./");
+#else
   char *subdir_name = SAFE_ALLOCA (len + 2);
   memcpy (subdir_name, dp->d_name, len);
   strcpy (subdir_name + len, "/");
+#endif
   bool dirp = faccessat (fd, subdir_name, F_OK, AT_EACCESS) == 0;
   SAFE_FREE ();
   return dirp;
