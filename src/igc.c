3d2d7b08c1afb3f7dc47602f846ca92b66f09852
Gerd Möllmann <gerd@gnu.org>
2024-02-23 19:55:21 +0100

    WIP

--- src/igc.c
+++ src/igc.c
@@ -18,35 +18,113 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>. */
 
-// clang-format off
+/* Random notes:
+
+- Use mps_arena_step during idle time. This lets MPS take a specified
+  maximum amount of time (default 10ms) for its work.
+
+*/
+
+// clang-format on
 
 #include <config.h>
 #include "lisp.h"
 
 #ifdef HAVE_MPS
+
+#if !USE_LSB_TAG
+# error "Need USE_LSB_TAG"
+#endif
+
 #include <stdlib.h>
 #include <mps.h>
 #include <mpsavm.h>
+#include "mpscamc.h"
 
 static mps_arena_t arena = NULL;
+static mps_chain_t chain;
+static mps_pool_t cons_pool;
+
+static mps_res_t
+cons_scan (mps_ss_t ss, mps_addr_t base, mps_addr_t limit)
+{
+  return MPS_RES_OK;
+}
+
+static mps_addr_t
+cons_skip (mps_addr_t addr)
+{
+  const struct Lisp_Cons *cons = addr;
+  return (mps_addr_t) (cons + 1);
+}
+
+static void
+cons_fwd (mps_addr_t old, mps_addr_t new)
+{
+  // unclear
+}
+
+static mps_addr_t
+cons_isfwd (mps_addr_t addr)
+{
+  return NULL;
+}
+
+static void
+cons_pad (mps_addr_t addr, size_t size)
+{
+}
 
 static void
 create_arena (void)
 {
   mps_res_t res;
+
   MPS_ARGS_BEGIN (args) {
-    MPS_ARGS_ADD (args, MPS_KEY_PAUSE_TIME, 0.01);
-    res = mps_arena_create_k(&arena, mps_arena_class_vm (), args);
+    res = mps_arena_create_k (&arena, mps_arena_class_vm (), args);
   } MPS_ARGS_END (args);
   if (res != MPS_RES_OK)
-    abort ();
+    emacs_abort ();
+
+  mps_gen_param_s gen_params[] = {
+    {32000, 0.8},
+    {5 * 32009, 0.4}
+  };
+  res = mps_chain_create(&chain, arena, ARRAYELTS (gen_params), gen_params);
+  if (res != MPS_RES_OK)
+    emacs_abort ();
+
+  /* Object format for conses.  */
+  mps_fmt_t cons_fmt;
+  MPS_ARGS_BEGIN (args) {
+    MPS_ARGS_ADD (args, MPS_KEY_FMT_ALIGN, 8);
+    MPS_ARGS_ADD (args, MPS_KEY_FMT_HEADER_SIZE, 0);
+    MPS_ARGS_ADD(args, MPS_KEY_FMT_SCAN, cons_scan);
+    MPS_ARGS_ADD(args, MPS_KEY_FMT_SKIP, cons_skip);
+    MPS_ARGS_ADD(args, MPS_KEY_FMT_FWD, cons_fwd);
+    MPS_ARGS_ADD(args, MPS_KEY_FMT_ISFWD, cons_isfwd);
+    MPS_ARGS_ADD(args, MPS_KEY_FMT_PAD, cons_pad);
+    res = mps_fmt_create_k(&cons_fmt, arena, args);
+  } MPS_ARGS_END(args);
+  if (res != MPS_RES_OK)
+    emacs_abort ();
+
+  /* Pool for conses. Since conses have no type field which would let us
+     recognize them when mixed with other objects, use a dedicated
+     pool.  */
+  MPS_ARGS_BEGIN (args) {
+    MPS_ARGS_ADD (args, MPS_KEY_FORMAT, cons_fmt);
+    MPS_ARGS_ADD (args, MPS_KEY_CHAIN, chain);
+    MPS_ARGS_ADD (args, MPS_KEY_INTERIOR, 0);
+    res = mps_pool_create_k (&cons_pool, arena, mps_class_amc (), args);
+  } MPS_ARGS_END(args);
+  if (res != MPS_RES_OK)
+    emacs_abort ();
 }
 
 static void
 destroy_arena (void)
 {
-  if (!arena)
-    return;
   mps_arena_destroy (arena);
 }
 
