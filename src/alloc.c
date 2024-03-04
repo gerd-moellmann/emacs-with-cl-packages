7841312fd8e2ee2d7b0c3fb9f16200b1b18ca88d
Gerd Möllmann <gerd@gnu.org>
2024-03-04 09:12:39 +0100

    Fix last merge

--- src/alloc.c
+++ src/alloc.c
@@ -3998,7 +3998,7 @@ Its value is void, and its function definition and property list are nil.  */)
     {
       ASAN_UNPOISON_SYMBOL (symbol_free_list);
       val = make_lisp_symbol (symbol_free_list);
-      symbol_free_list = symbol_free_list->u.s.next;
+      symbol_free_list = next_free_symbol (symbol_free_list);
     }
   else
     {
