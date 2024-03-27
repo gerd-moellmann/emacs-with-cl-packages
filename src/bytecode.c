556d7f2ca48508138ec7ac67c755cf9d7be722bb
Gerd Möllmann <gerd@gnu.org>
2024-03-27 13:39:00 +0100

    igc_malloc byte stack

--- src/bytecode.c
+++ src/bytecode.c
@@ -21,6 +21,7 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include "lisp.h"
 #include "blockinput.h"
+#include "igc.h"
 #include "sysstdio.h"
 #include "character.h"
 #include "buffer.h"
@@ -389,7 +390,11 @@ struct bc_frame {
 void
 init_bc_thread (struct bc_thread_state *bc)
 {
+#ifdef HAVE_MPS
+  bc->stack = igc_xmalloc (BC_STACK_SIZE);
+#else
   bc->stack = xmalloc (BC_STACK_SIZE);
+#endif
   bc->stack_end = bc->stack + BC_STACK_SIZE;
   /* Put a dummy header at the bottom to indicate the first free location.  */
   bc->fp = (struct bc_frame *)bc->stack;
@@ -399,7 +404,11 @@ init_bc_thread (struct bc_thread_state *bc)
 void
 free_bc_thread (struct bc_thread_state *bc)
 {
+#ifdef HAVE_MPS
+  igc_xfree (bc->stack);
+#else
   xfree (bc->stack);
+#endif
 }
 
 #ifndef HAVE_MPS
