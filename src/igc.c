38f19e3dee3c4ed9cf713efc2069bf988c8164c8
Gerd Möllmann <gerd@gnu.org>
2024-04-02 13:23:17 +0200

    Finalize mutex

--- src/igc.c
+++ src/igc.c
@@ -1462,6 +1462,10 @@ finalize_vector (mps_addr_t v)
       finalize_one_thread (v);
       break;
 
+    case PVEC_MUTEX:
+      finalize_one_mutex (v);
+      break;
+
     case PVEC_SYMBOL_WITH_POS:
     case PVEC_PROCESS:
     case PVEC_RECORD:
@@ -1490,7 +1494,6 @@ finalize_vector (mps_addr_t v)
     case PVEC_USER_PTR:
     case PVEC_XWIDGET:
     case PVEC_XWIDGET_VIEW:
-    case PVEC_MUTEX:
     case PVEC_TERMINAL:
     case PVEC_MARKER:
       igc_assert (!"not implemented");
@@ -1540,6 +1543,7 @@ maybe_finalize (mps_addr_t client, enum pvec_type tag)
     case PVEC_BIGNUM:
     case PVEC_FONT:
     case PVEC_THREAD:
+    case PVEC_MUTEX:
       mps_finalize (global_igc->arena, &ref);
       break;
 
