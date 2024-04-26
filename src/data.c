9db2a0af780f40749c6492109ec87e0f4f413d6d
Gerd Möllmann <gerd@gnu.org>
2024-04-26 16:30:25 +0200

    Use PVEC_MODULE_GLOBAL_REFERENCE instead of PVEC_OTHER

--- src/data.c
+++ src/data.c
@@ -235,6 +235,7 @@ a fixed set of types.  */)
       /* WARNING!!  Keep 'cl--type-hierarchy' in sync with this code!!  */
       switch (PSEUDOVECTOR_TYPE (XVECTOR (object)))
 	{
+	case PVEC_MODULE_GLOBAL_REFERENCE: return Qmodule_global_reference;
         case PVEC_NORMAL_VECTOR: return Qvector;
 	case PVEC_BIGNUM: return Qbignum;
 	case PVEC_MARKER: return Qmarker;
@@ -4228,6 +4229,7 @@ syms_of_data (void)
   DEFSYM (Qcons, "cons");
   DEFSYM (Qmarker, "marker");
   DEFSYM (Qsymbol_with_pos, "symbol-with-pos");
+  DEFSYM (Qmodule_global_reference, "module-global-reference");
   DEFSYM (Qoverlay, "overlay");
   DEFSYM (Qfinalizer, "finalizer");
   DEFSYM (Qmodule_function, "module-function");
--- src/pdumper.c
+++ src/pdumper.c
@@ -3116,6 +3116,7 @@ dump_vectorlike (struct dump_context *ctx,
     case PVEC_CONDVAR:
     case PVEC_SQLITE:
     case PVEC_MODULE_FUNCTION:
+    case PVEC_MODULE_GLOBAL_REFERENCE:
     case PVEC_SYMBOL_WITH_POS:
     case PVEC_FREE:
     case PVEC_TS_PARSER:
