97adcda990c1394535213b806bc1646fecdcef13
Gerd Möllmann <gerd@gnu.org>
2024-05-26 09:11:00 +0200

    Fixi non-MPS build

--- src/pdumper.c
+++ src/pdumper.c
@@ -2786,7 +2786,9 @@ dump_object_array (struct dump_context *ctx,
   struct dump_flags old_flags = ctx->flags;
   ctx->flags.pack_objects = true;
 
+#ifdef HAVE_MPS
   dump_igc_start_obj (ctx, IGC_OBJ_OBJ_VEC, array);
+#endif
   dump_off start_offset = ctx->offset;
 
   for (size_t i = 0; i < len; i++)
@@ -2799,8 +2801,9 @@ dump_object_array (struct dump_context *ctx,
     }
 
   ctx->flags = old_flags;
+#ifdef HAVE_MPS
   dump_igc_finish_obj (ctx);
-
+#endif
   return start_offset;
 }
 
