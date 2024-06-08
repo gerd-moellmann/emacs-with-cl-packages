f66d0ca52abc09f268b02a68274dbffebd2788e8
Gerd Möllmann <gerd@gnu.org>
2024-06-08 09:57:59 +0200

    More IGC_OBJ_BUILTIN types

--- src/igc.h
+++ src/igc.h
@@ -49,6 +49,8 @@ enum igc_obj_type
   IGC_OBJ_HANDLER,
   IGC_OBJ_BYTES,
   IGC_OBJ_BUILTIN_SYMBOL,
+  IGC_OBJ_BUILTIN_THREAD,
+  IGC_OBJ_BUILTIN_SUBR,
   IGC_OBJ_NUM_TYPES
 };
 
@@ -158,7 +160,8 @@ XWEAK_REF (Lisp_Object a)
 }
 
 size_t igc_header_size (void);
-char *igc_dump_finish_obj (void *client, enum igc_obj_type type, char *base, char *end);
+char *igc_dump_finish_obj (void *client, enum igc_obj_type type,
+			   char *base, char *end);
 
 # define eassert_not_mps() eassert (false)
 #else
