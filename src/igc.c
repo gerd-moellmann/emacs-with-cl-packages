b21f651a2c117a5015fde67e314ed999d45592a4
Gerd Möllmann <gerd@gnu.org>
2024-12-25 07:36:33 +0100

    Revert "Ifndef new igc code"
    This reverts commit b10b137a202171ed19c9bd9c7dde946809b31ec9.

--- src/igc.c
+++ src/igc.c
@@ -4984,7 +4984,6 @@ igc_busy_p (void)
 bool
 gc_signal_handler_can_run (int sig)
 {
-#ifndef IN_MY_FORK
   eassume (sig >= 0);
   eassert (sig < ARRAYELTS (global_igc->pending_signals));
   if (igc_busy_p ())
@@ -4999,7 +4998,6 @@ gc_signal_handler_can_run (int sig)
        * it doesn't work.  */
       return false;
     }
-#endif
   return true;
 }
 
@@ -5007,7 +5005,6 @@ gc_signal_handler_can_run (int sig)
 void
 gc_maybe_quit (void)
 {
-#ifndef IN_MY_FORK
   while (global_igc->signals_pending)
     {
       global_igc->signals_pending = 0;
@@ -5019,7 +5016,6 @@ gc_maybe_quit (void)
 	  }
       pthread_sigmask (SIG_SETMASK, &global_igc->signal_mask, NULL);
     }
-#endif
 }
 
 DEFUN ("igc--add-extra-dependency", Figc__add_extra_dependency,
