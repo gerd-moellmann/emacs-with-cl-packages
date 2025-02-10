26c93a325507d56e4a1573271d6dee44298caefa
Gerd Möllmann <gerd,moellmann@gmail.com>
2025-02-10 16:15:26 +0100

    Offset sub-pane a little bit
    * lisp/tty-menu.el (tty-menu-where): Subtract a bit from x.

--- lisp/tty-menu.el
+++ lisp/tty-menu.el
@@ -619,9 +619,9 @@ buffer, and HEIGHT is the number of lines in the buffer. "
   (cl-ecase how
     ((mouse key)
      (let* ((posn (posn-at-point (line-end-position)))
-	    (xy (posn-x-y posn))
 	    (win (posn-window posn)))
-       (tty-menu-position (list xy win))))))
+       (cl-destructuring-bind (x . y) (posn-x-y posn)
+         (tty-menu-position (list (cons (- x 3) y) win)))))))
 
 (defun tty-menu-loop (keymap where)
   (let ((frame (tty-menu-create-frame keymap where)))
