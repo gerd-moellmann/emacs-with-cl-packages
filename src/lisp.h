da4d652f96bf9c7b87812533014d3a565a93f755
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-03-01 16:25:51 +0100

    Revert "Iterating over markers in most-recently added order"
    This reverts commit 0a46a804048bf89fdd1698ce46cf02535faebbaa.

--- src/lisp.h
+++ src/lisp.h
@@ -3083,7 +3083,7 @@ struct Lisp_Marker
 # ifdef HAVE_MPS
   /* If in a buffer's marker vector, this is the index where it is
      stored. */
-  ptrdiff_t slot;
+  ptrdiff_t index;
 # endif
 } GCALIGNED_STRUCT;
 
