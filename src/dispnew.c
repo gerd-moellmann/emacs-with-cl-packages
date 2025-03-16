a10441ec84e3fe4b7d9533bda43d11d892dc716a
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-03-16 21:17:55 +0100

    Make sure to initialize glyph::frame to NULL (bug#77039)
    * src/dispnew.c (adjust_glyph_matrix): Clear glyph memory when
    enlarging window-system window glyph matrices.

--- src/dispnew.c
+++ src/dispnew.c
@@ -504,9 +504,11 @@ adjust_glyph_matrix (struct window *w, struct glyph_matrix *matrix, int x, int y
 
 	  while (row < end)
 	    {
-	      row->glyphs[LEFT_MARGIN_AREA] =
-		xnrealloc (row->glyphs[LEFT_MARGIN_AREA],
-			   dim.width, sizeof (struct glyph));
+	      row->glyphs[LEFT_MARGIN_AREA]
+		= xnrealloc (row->glyphs[LEFT_MARGIN_AREA], dim.width,
+			     sizeof (struct glyph));
+	      memset (row->glyphs[LEFT_MARGIN_AREA], 0,
+		      dim.width * sizeof (struct glyph));
 	      /* The mode line, if displayed, never has marginal areas.  */
 	      if ((row == matrix->rows + dim.height - 1
 		   && !(w && window_wants_mode_line (w)))
