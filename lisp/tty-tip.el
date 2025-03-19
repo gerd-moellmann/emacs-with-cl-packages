e3ef9be63b3c7810a93afb11ee8be80b16f8aff2
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-03-19 21:39:07 +0100

    Revert "; Don't set symbol-packages in tty-tip.el"
    This reverts commit beeece4712c464a6eb62c8e9b72c6f7515614f6c.

--- lisp/tty-tip.el
+++ lisp/tty-tip.el
@@ -1,4 +1,4 @@
-;;; tty-tip.el --- Display help in kind of tooltips on ttys  -*- lexical-binding: t -*-
+;;; tty-tip.el --- Display help in kind of tooltips on ttys  -*- lexical-binding: t; symbol-packages: t; -*-
 
 ;; Copyright (C) 2024-2025 Free Software Foundation, Inc.
 
