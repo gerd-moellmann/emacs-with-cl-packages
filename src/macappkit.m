3559cbd22b54619b031601e623b93d2b8c67cc95
YAMAMOTO Mitsuharu <mituharu@math.s.chiba-u.ac.jp>
2025-03-31 14:12:01 +0900

    Update copyright year to 2025

--- src/macappkit.m
+++ src/macappkit.m
@@ -1,5 +1,5 @@
 /* Functions for GUI implemented with Cocoa AppKit on macOS.
-   Copyright (C) 2008-2024  YAMAMOTO Mitsuharu
+   Copyright (C) 2008-2025  YAMAMOTO Mitsuharu
 
 This file is part of GNU Emacs Mac port.
 
