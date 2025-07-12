c578fafba029c745ddab081f3258ca9f3c9bff1b
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-07-12 09:36:19 +0200

    * src/ncterm.h (ncterm_display_info): Add.

--- src/ncterm.h
+++ src/ncterm.h
@@ -1,7 +1,16 @@
 #ifndef EMACS_NCTERM_H
-#define EMACS_NCTERM_H
+# define EMACS_NCTERM_H
+
+struct ncterm_display_info
+{
+    struct terminal *terminal;
+    struct notcurses *nc;
+    const char *name;
+};
 
 void init_ncterm (void);
 void syms_of_ncterm (void);
+struct terminal *ncterm_init_terminal (const char *name, const char *terminal_type,
+    bool must_succeed);
 
 #endif
