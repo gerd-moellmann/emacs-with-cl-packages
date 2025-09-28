554e0f99a9f8f011c0aaf3c91946d36f5ee5666f
Ben Simms <ben@bensimms.moe>
2025-09-28 10:57:24 +0200

    Fix bad merge

--- src/emacs.c
+++ src/emacs.c
@@ -3443,8 +3443,6 @@ decode_env_path (const char *evarname, const char *defalt, bool empty)
 #ifdef MAC_SELF_CONTAINED
       path = mac_relocate (defalt);
 #elif defined NS_SELF_CONTAINED
-      /* ns_relocate needs a valid autorelease pool around it.  */
-      autorelease = ns_alloc_autorelease_pool ();
       path = ns_relocate (defalt);
 #else
       path = defalt;
