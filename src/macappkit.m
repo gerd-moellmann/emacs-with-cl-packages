37cd0b8cd8c24843e5ae3d0052a39409552922f0
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-10-22 15:12:32 +0200

    Reapply "Disable NSAutoFillHeuristicController"
    This reverts commit 9eba333ae0d218bed7b4751868fad148411d61e2.

--- src/macappkit.m
+++ src/macappkit.m
@@ -6512,6 +6512,8 @@ static BOOL emacsViewUpdateLayerDisabled;
     {
       NSUserDefaults *defaults = NSUserDefaults.standardUserDefaults;
 
+      if ([defaults objectForKey:@"NSAutoFillHeuristicControllerEnabled"] == nil)
+	[defaults registerDefaults:@{@"NSAutoFillHeuristicControllerEnabled" : @false}];
       if ([defaults objectForKey:@"ApplePressAndHoldEnabled"] == nil)
 	[defaults registerDefaults:@{@"ApplePressAndHoldEnabled" : @"NO"}];
     }
