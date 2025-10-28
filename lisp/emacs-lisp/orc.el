5e1f592e0b76e2ce03ff6f334a9d363eb1820ac2
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-10-28 06:21:01 +0100

    New file emacs-lisp/orc.el

---
+++ lisp/emacs-lisp/orc.el
@@ -0,0 +1,80 @@
+;;; orc.el --- LLVM ORC jit compilation of Lisp code -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Native compilation uses the GCC jit compilation library. The Lisp
+;; part of the native compiler translates byte code to an IR that is
+;; translated to GCC jit IR in comp.c in a last pass (function
+;; comp--final).
+;;
+;; For ORC, we reuse the native compiler except its last pass
+;; comp--final. We translate the native compiler's IR to C which we can
+;; pass to LLVM ORC. The intermediate C representation is used because
+;; it is much simpler to generate.
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'cl-macs)
+(require 'comp)
+
+(cl-defstruct orc
+  "Context for an ORC compilation."
+  (comp-ctxt nil))
+
+(defvar orc--indent-width 4
+  "Indentation width to use.")
+
+(defvar orc--indent-level ß
+  "Current indentation level.")
+
+(defvar orc-ctxt nil
+  "Current ORC compilation context")
+
+(defmacro with-orc-indentation (&body body)
+  "Execute BODY with i`org--indent-level' incremented."
+  `(let ((orc--indent-level (1+ orc--indent-level)))
+     ,@body))
+
+(defun orc-format (format &rest args)
+  "Print an indented line to the current buffer.
+FORMAT is a format string for `format', and ARGS are arguments for it.
+Indentation is according to the current value of `org--indent-level'."
+  (indent-to (* orc--indent-level orc--indent-width))
+  (insert (apply #'format format args) "\n"))
+
+(defun orc--final-pass (ctxt)
+  "Final native compiler pass for ORC."
+  (let ((orc-ctxt (make-orc :comp-ctxt ctxt)))
+    orc-ctxt))
+
+(defun orc--around-comp--final (_old-fun &rest _args)
+  "Around-advice for `comp--final'."
+  (cl-assert (comp-ctxt-p comp-ctxt))
+  (orc--final-pass comp-ctxt))
+
+(define-minor-mode orc-mode
+  "Global minor mode ORC native compilation."
+  :global t :group 'lisp
+  (if orc-mode
+      (advice-add 'comp--final :around #'orc--around-comp--final)
+    (advice-remove 'comp--final #'orc--around-comp--final)))
+
+(provide 'orc)
